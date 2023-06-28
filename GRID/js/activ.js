ee240fa7cdaf443e6a7c070c35225dcea73b3c6b
Gui784 <guillaumek53@gmail.com>
2023-06-28 16:56:23 +0200

    cours

---
+++ GRID/js/activ.js
@@ -0,0 +1,6 @@
+// Navigation avec les liens actif sur la page 
+
+// 1- Ajout de l'ID #activ_nav sur la balise nav 
+// 2- Vérifier que la classe .activ est bien dans le fichier CSS
+// 3- Créer la fonction setActiv() pour ajouter la class .activ sur le lien URL en cours
+
