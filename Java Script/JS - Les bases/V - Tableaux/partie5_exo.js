5c073a82d86cc1ca3b15524243a1d1f89a01a3af
Gui784 <guillaumek53@gmail.com>
2023-07-19 16:57:09 +0200

    cours

---
+++ Java Script/JS - Les bases/V - Tableaux/partie5_exo.js
@@ -0,0 +1,56 @@
+// [ source: https://github.com/oc-courses/intro-javascript ]
+
+
+/*
+* EXERCICE 1 - les tableaux
+* Complétez le programme pour calculer puis afficher la somme des valeurs du tableau nombres.
+*/
+
+var nombres = [11, 3, 7, 2, 9, 10];
+
+var somme = 0;
+
+nombres.forEach(function (nbr) {
+
+    somme = somme + nbr;
+
+} )
+ console.log(' la somme des chiffres du tableau et de : ' + somme);
+
+/*
+* EXERCICE 2 - les tableaux
+* Complétez le programme pour qu'il calcule et affiche ensuite la plus grande valeur présente dans le tableau.
+*/
+
+let valeurs = [3, 11, 7, 2, 9, 10];
+
+let max = valeurs[0];
+
+for (let i = 1; i < valeurs.length; i++){
+
+    if (valeurs[i]);
+
+}
+
+/*
+* EXERCICE 3 - les tableaux
+* Ecrivez un programme qui crée un tableau contenant les noms des trois mousquetaires, Athos, Porthos et Aramis ;
+* 1- Affiche le nom de chaque mousquetaire à l'aide d'une boucle for ;
+* 2- Ajoute au tableau le mousquetaire d'Artagnan ;
+* 3- Affiche de nouveau le nom de chaque mousquetaire, cette fois à l'aide de la méthode forEach().
+*/
+
+let mousquetaires = ["Athos", "Porthos", "Aramis"];
+
+
+/*
+* EXERCICE 4 - les tableaux
+* Ecrivez un programme qui fait saisir à l'utilisateur des mots jusqu'à ce qu'il saisisse "stop".
+* Le programme affiche alors la liste des mots saisis.
+*/
+
+// code 
+
+
+
+
