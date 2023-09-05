54f260d0fe44c5e1871af6c76f49567c28684c3d
Gui784 <guillaumek53@gmail.com>
2023-09-05 16:54:18 +0200

    cours

--- PHP/14-PDO_connexion/connexion.php
+++ PHP/14-PDO_connexion/connexion.php
@@ -1,27 +1,40 @@
-<?php
 
-include "../Nav_global/nav.php";
+<!-- le passage php : < ?php, permet d'entrer du code php sur une page HTML-->
+<?php 
 
-// PDO est une surcouche de PHP et directement intégrés à ce dernier depuis PHP 5.1
+// PDO est une surcouche de PHP, directement intégrée à ce dernier depuis PHP version 5.1.
 
-// PDO permete de connecter notre projet à sa base de données , puis de mêler du langage PHP avec de srequetes en SQL
+// PDO permet de connecter notre projet à sa base de données, puis de mêler langage php avec des requetes en sql
 
-// PDO est l'abréviation de PHP Data Object est une extension définissant l'interface pour accéder à une base données avec PHP . Elle est orientée objet.
+// PDO= PHP Data Object, c'est une extension definissant l'interface pour accéder à une base de données avec PHP. Elle est orientée objet.
 
-// La connexion avec la base de données va être différente en fonctionn de l'hébergeur dans notre cas notre base de données est en local
+// la connexion avec la base de données va être différente en fonction de l'hébergeur. Dans notre cas notre base de données est en local.
 
-$pdo = new PDO('mysql:host=localhost; dbname=haribo','root' , '' , array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+// $pdo = new PDO('mysql:host=localhost; dbname=haribo', 'root', ''); Cela crée une nouvelle instance PDO en se connectant à une base de données MySQL sur le serveur localhost avec le nom de la base de données haribo. Les identifiants d'utilisateur et de mot de passe (root et '' dans cet exemple) sont également fournis.
+
+/* array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+)
+*/
+
+/* Ici un tableau associatif d'options à PDO lors de sa création. Deux options sont spécifiées :
+
+PDO::ATTR_ERRMODE est défini sur PDO::ERRMODE_WARNING, ce qui signifie que les erreurs seront gérées en affichant des alertes de type "warning".
+PDO::MYSQL_ATTR_INIT_COMMAND est défini sur 'SET NAMES utf8', ce qui indique que la commande MySQL pour l'initialisation de la connexion sera SET NAMES utf8. Cela permet de définir le jeu de caractères de la connexion sur UTF-8 pour prendre en charge des caractères spéciaux.*/ 
+$pdo = new PDO('mysql:host=localhost; dbname=haribo', 'root', '', array (PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' ));
 
 echo "<pre>";
-
-// get_class_methods permet de récupérer toutes les methods disponible avec l'objet
-
 print_r(get_class_methods($pdo));
+//  get_class_methods Cela te permettra d'avoir un aperçu des méthodes que tu peux utiliser avec l'objet PDO pour interagir avec la base de données.
+
+// pour plus d'infos: https://www.php.net/manual/fr/class.pdo.php
+echo"</pre>";
 
 
-echo "</pre>";
-
-$req = $bdd->prepare('SELECT*FROM bonbon');
+// Requête SQL pour sélectionner toutes les lignes de la table "bonbons"
+$query = "SELECT * FROM bonbon";
 
 try {
     // Exécute la requête et récupère les résultats
@@ -39,7 +52,7 @@ try {
 
 
 // Requête SQL pour sélectionner toutes les lignes de la table "bonbons"
-$query = "SELECT * FROM bonbons";
+$query = "SELECT * FROM bonbon";
 
 try {
     // Prépare la requête
---
+++ php_cours2/website/index.php
@@ -0,0 +1,235 @@
+<?php
+                $adminInfo = file_get_contents('logs/donnees.json', true);
+                $adminArray = json_decode($adminInfo , true);
+
+                print_r($adminArray);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Shop Homepage - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!"><?php echo $_POST['title'] ?></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
+                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            Cart
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="zbi bg-dark py-5" style="background-color: <?php echo $_POST['color'] ."!important";?>">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder" ><?php echo $_POST['title_header'] ?></h1>
+                    <p class="lead fw-normal text-white-50 mb-0"><?php echo $_POST['subtitle'] ?></p>
+                </div>
+            </div>
+        </header>
+        <!-- Section-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+
+                <?php
+
+                  echo "Ok";
+
+                ?>
+
+                <form method="post" id="form_user">
+
+                    <label for="email">Email : </label>
+                    <input type="email" id="email" name="email" placeholder="Insérez votre Email">
+
+                    <br>
+
+                    <label for="firstname">Prénom</label>
+                    <input type="text" id="firstname" name="firstname" placeholder="Votre Prénom">
+
+                    <br>
+
+                    <label for="lastname">Nom</label>
+                    <input type="text" id="lastname" name="lastname">
+
+                    <br>
+
+                    <input type="color" name="color">
+
+                    <br>
+
+                    <input type="submit" value="Je m'inscris" name="validate">
+
+                </form>
+
+                <br>
+                <br>
+
+                <form method="POST" id="formu">
+
+                <label for="color">Couleur du header</label>
+
+                <br>
+                <input type="text" id="title" name="title" placeholder="Titre du site">
+                <label type='text'></label>                
+                <br>
+                <input type="text" id="title_header" name="title_header" placeholder="Titre du header">
+                <label type='text'></label>
+                <br>
+                <input type="text" id="subtitle" name="subtitle" placeholder="Sous titre du header">
+                <label type='text'></label>
+                <br>
+                <input type="file" id="pictures" name="pictures" placeholder="Image du produit">
+                <label type='file'></label>
+                <br>
+                <input type="color" value="color" name="color">
+                <input type="submit" value="Mettre à jour">
+
+                <?php
+
+                if(!empty($_POST)){
+
+
+                    if(!empty($_POST['title_header']) && !empty($_POST['title']) && !empty('subtitle'));
+
+                $title_header = $_POST['title_header'];  
+                $title = $_POST['title']; 
+                $subtitle = $_POST['subtitle']; 
+
+
+                $data = array(
+                 'color' => $_POST['color'],
+                 'titre_header' => $_POST['title_header'],                      
+                 'sous_titre' => $_POST['subtitle'],
+                 'titre_site' => $_POST['title'],
+                );
+
+                $jsonData = json_encode($data);
+
+                $nomJSON = fopen('logs/donnees.json','w');
+                fwrite($nomJSON , $jsonData);
+                fclose($nomJSON);
+
+
+
+
+
+
+                }
+
+
+
+                ?>
+
+
+                </form>
+
+
+                <!-- <?php
+
+                // echo "<pre>";
+                //  print_r($_POST);
+                // echo "</pre>"; -->
+
+
+                // Si mon prénom fait moins de 5 charactéres affiche un message d'erreur
+
+                // $prenom = $_POST["firstname"];
+                // $color = $_POST["color"];
+
+
+                // if (strlen($prenom) < 5){
+
+                //    echo "non";
+
+                // } else {
+
+                //     echo "oui";
+
+                //     $file = fopen('logs/couleur.txt','a');
+                //     $log = "La couleur choisis est : " . $color ;
+
+                //     fwrite($file ,$color);
+
+                //     fclose($file);
+
+                //     echo "<style>.zbi{background-color:$color!important}</style>";
+                // }
+
+
+                // ?>
+
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Product image-->
+                            <img class="card-img-top" src="<?php echo $_POST['pictures'] ?>" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder">Popular Item</h5>
+                                    <!-- Product reviews-->
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    <!-- Product price-->
+                                    $40.00
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
